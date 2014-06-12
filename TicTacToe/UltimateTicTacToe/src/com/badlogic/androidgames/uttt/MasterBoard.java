b573f94f72725f9de0acae0d96c34037f5a47a65
RyanJeff <ryan.feyen33@gmail.com>
2014-06-12 12:10:58 -0400

    Updated Packages
    gejinrhdi0

--- TicTacToe/UltimateTicTacToe/src/com/badlogic/androidgames/uttt/MasterBoard.java
+++ TicTacToe/UltimateTicTacToe/src/com/badlogic/androidgames/uttt/MasterBoard.java
@@ -1,35 +1,18 @@
 package com.badlogic.androidgames.uttt;
 
 //import java.util.Random;
-
+import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+ 
+import javax.swing.*;
+ 
 public class MasterBoard
 {
-	
-
-	//model of the board
-    /*
-    static final int SEC1_WIDTH = 133;
-    static final int SEC2_WIDTH = 133;
-    static final int SEC3_WIDTH = 133;
-    static final int SEC4_WIDTH = 133;
-    static final int SEC5_WIDTH = 133;
-    static final int SEC6_WIDTH = 133;
-    static final int SEC7_WIDTH = 133;
-    static final int SEC8_WIDTH = 133;
-    static final int SEC9_WIDTH = 133;
-    static final int SEC1_HEIGHT = 133;
-    static final int SEC2_HEIGHT = 133;
-    static final int SEC3_HEIGHT = 133;
-    static final int SEC4_HEIGHT = 133;
-    static final int SEC5_HEIGHT = 133;
-    static final int SEC6_HEIGHT = 133;
-    static final int SEC7_HEIGHT = 133;
-    static final int SEC8_HEIGHT = 133;
-    static final int SEC9_HEIGHT = 133;
-    */
-
-    public boolean gameOver = false;
-
+	public boolean gameOver = false;
+    
     public class MiniBoard 
     {
     	
