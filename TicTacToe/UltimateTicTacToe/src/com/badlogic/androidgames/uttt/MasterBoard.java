66cf59529e373e50826b3fd8db95d27d4efcbd1c
RyanJeff <ryan.feyen33@gmail.com>
2014-06-12 10:09:54 -0400

    Update

---
+++ TicTacToe/UltimateTicTacToe/src/com/badlogic/androidgames/uttt/MasterBoard.java
@@ -0,0 +1,50 @@
+package com.badlogic.androidgames.uttt;
+
+//import java.util.Random;
+
+public class MasterBoard
+{
+	
+
+	//model of the board
+    /*
+    static final int SEC1_WIDTH = 133;
+    static final int SEC2_WIDTH = 133;
+    static final int SEC3_WIDTH = 133;
+    static final int SEC4_WIDTH = 133;
+    static final int SEC5_WIDTH = 133;
+    static final int SEC6_WIDTH = 133;
+    static final int SEC7_WIDTH = 133;
+    static final int SEC8_WIDTH = 133;
+    static final int SEC9_WIDTH = 133;
+    static final int SEC1_HEIGHT = 133;
+    static final int SEC2_HEIGHT = 133;
+    static final int SEC3_HEIGHT = 133;
+    static final int SEC4_HEIGHT = 133;
+    static final int SEC5_HEIGHT = 133;
+    static final int SEC6_HEIGHT = 133;
+    static final int SEC7_HEIGHT = 133;
+    static final int SEC8_HEIGHT = 133;
+    static final int SEC9_HEIGHT = 133;
+    */
+
+    public boolean gameOver = false;
+
+    public class MiniBoard 
+    {
+    	
+	}
+    
+    public MasterBoard() 
+    {
+        
+    }
+    
+    public void update(float deltaTime) 
+    {
+        if (gameOver)
+        {
+            return;
+        }
+    }
+}
